855e1c19404907cc0ceafe39a71c36f97e28cdbc
Edgar Zaganjori <edgarzaganjori@gmail.com>
2015-11-05 16:22:21 -0500

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,8 @@
 
 /**
- * @author Richmond Frimpong, Edgar Zaganjori, Daniyal Javed cse : cse23004,
- *         cse23106, cse31034 YorkU Parking Meter GUI EECS3461 - Scott McKenzie
+ * @author Richmond Frimpong, Edgar Zaganjori, Daniyal Javed 
+ * YorkU Parking Meter GUI 
+ * EECS3461 - Scott McKenzie
  */
 
 public class Person {
